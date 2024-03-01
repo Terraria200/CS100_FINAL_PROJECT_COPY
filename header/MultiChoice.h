6aa5b86dbb57ac3036dd24b421c5a8190bc37fe1
nicholasstermer <nster005@ucr.edu>
2024-03-01 14:25:42 -0800

    MutltiChoice class almost finished just need to fix GetAnswer function

--- header/MultiChoice.h
+++ header/MultiChoice.h
@@ -1,11 +1,10 @@
-#include <string>
 #include "../header/Question.h"
 
-class MultiChoice: public Question {
+class MultiChoice : public Question {
     private:
-        
-    
+        std::string answer;
     public:
-        MultiChoice();
-    
+        MultiChoice(std::string content, int score, std::string answer);
+        bool checkAnswer(std::string answer);
+        std::string GetAnswer();
 };
--- src/MultiChoice.cpp
+++ src/MultiChoice.cpp
@@ -1,5 +1,24 @@
 #include "../header/MultiChoice.h"
+#include <algorithm>
 
-MultiChoice::MultiChoice() {
-    
+MultiChoice::MultiChoice(std::string content, int score, std::string answer) : Question(content, answer, score) {
+    this->answer = answer;
+}
+
+bool MultiChoice::checkAnswer(std::string answer) {
+    std::transform(answer.begin(), answer.end(), answer.begin(), ::tolower);
+
+    if (answer == getAnswer()) {
+        return "true";
+    }
+    else if (answer != getAnswer()) {
+        return "false";
+    }
+    else {
+        return "false";
+    }
+}
+
+std::string MultiChoice::GetAnswer() {
+    return this->answer;
 }
