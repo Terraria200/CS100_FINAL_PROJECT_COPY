bd3ae30249c47a1eb1815e599f3d19dc07cef897
Terraria200 <ahamedharoon52@gmail.com>
2024-03-10 12:56:48 -0700

    Commiting Quiz Constructor files to GitHub again

---
+++ header/QuizConstructor.h
@@ -0,0 +1,19 @@
+// We put header guards at beginning and end of this file to ensure that we only create it if it's not defined
+#ifndef QUIZCONSTRUCTOR_H
+#define QUIZCONSTRUCTOR_H
+
+#include<cstdlib>
+using namespace std;
+
+class QuizConstructor{
+    private: // Pretty sure that generally, variables are private while functions are public
+        Quiz *quiz;
+    public:
+        QuizConstructor();
+        Quiz *JsonToQuiz(string filename);
+        void setAnswer();
+        void editQuestion();
+}
+
+
+#endif
---
+++ src/QuizConstructor.cpp
@@ -0,0 +1,61 @@
+#include "../header/QuizConstructor.h"
+
+QuizConstuctor::QuizConstructor(){
+}
+
+Quiz * QuizConstructor::JsonToQuiz(string filename){
+}
+
+void QuizConstructor::setAnswer(){
+    getline(cin, correctAnswer);
+}
+
+void QuizConstructor::editQuestion(){
+    int questionToEdit;
+    char menuChoice = 'g';
+    char answerToEdit;
+    while(menuChoice != 'q'){
+        cout << "Select a question to edit from 1- " << questions.size() << ": ";
+        cin >> questionToEdit;
+        cout << endl;
+        while(menuChoice != 'm'){
+            if(questionToEdit >= 1 && questionToEdit <= questions.size()){
+                cout << "Select 'q' to edit the question, 'a' to edit the answers, 'm' to quit: ";
+                cin >> menuChoice;
+                cout << endl;
+                if(menuChoice == 'q'){
+                    cout << "Old question is \"" << questions[questionToEdit]->content << "\" Enter the new or edited question: "
+                    getline(cin, questions[questionToEdit]->content);
+                    cout << endl;
+                    cout << "Changes saved! Select 'q' to edit the question, 'a' to edit the answers, 'm' to quit: ";
+                }
+                else if(menuChoice == 'a'){
+                    cout << "Which options would you like to edit? Press 'q' when ready to quit editing" << endl;
+
+                    char alphabet = 'a';
+                    while(menuChoice != 'q'){
+                        for(unsigned int i = 0; i < options.size(); ++i){
+                            cin >> answerToEdit;
+                            cout << alphabet << ". " << options[i] << endl;
+                            if(answerToEdit == alphabet){
+                                cout << "What would you like to change it too? ";
+                                getline(cin, options[i]);
+                                cout << "Changes saved! Which options would you like to edit? Press 'q' when ready to quit editing" << endl;
+                            }
+                        ++alphabet;
+                    }
+                    }
+                }
+                else if(menuChoice == 'm'){
+                    menuChoice = 'q'; // Quit out of the editing process
+                }
+                else {
+                    menuChoice == 'b'; // Not a valid question number, so try edit process again
+                }
+            }
+            else{
+                menuChoice= 'b'; // Not a valid question number, so try edit process again
+            }
+        }
+    }
+}
