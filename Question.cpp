b3dff7aefc5569dc10af28fd527ceeecd7db7b8c
Haroon Ahamed <56659007+Terraria200@users.noreply.github.com>
2024-03-10 12:48:27 -0700

    Remove Haroon's changes for Question.cpp

--- Question.cpp
+++ Question.cpp
@@ -1,19 +1,1 @@
-#include "Question.h"
 
-boolean MultiChoice::checkAnswer(){
-    if(inputAnswer == correctAnswer){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-void MultiChoice::changeScore(){
-    if(checkAnswer() == true){
-        ++score;
-    }
-    else{
-        --score;
-    }
-}
